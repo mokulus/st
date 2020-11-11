0831481c5469d75aa0e6915c217945b9bc529610
Mateusz Okulus <mmokulus@gmail.com>
2020-11-11 16:32:45 +0100

    Decrease font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=18";
+static char *font = "monospace:pixelsize=16";
 static int borderpx = 0;
 static int disablebold = 0;
 static int disableitalic = 0;
